6f25282811eec5849f879ea8221f1eb4fcc41dfc
MilkFansHelloWorld <tcw57@cam.ac.uk>
2021-02-25 18:49:40 +0000

    changed graph

--- solvers/solve.py
+++ solvers/solve.py
@@ -12,19 +12,24 @@ from queue import Queue
 
 class Graph:
     def __init__(self, edges):
-        self.adj_list = {}
+        self.adj_list_from_B = {}
+        self.adj_list_to_E = {}
         for street in edges:
-            if street.B not in self.adj_list:
-                self.adj_list[street.B] = []
-            self.adj_list[street.B].append((street, Queue()))
-        self.vertices = self.adj_list.keys()
+            if street.B not in self.adj_list_from_B:
+                self.adj_list_from_B[street.B] = []
+            street_queue_couple = (street, Queue())
+            self.adj_list_from_B[street.B].append(street_queue_couple)
+            if street.E not in self.adj_list_to_E:
+                self.adj_list_from_B[street.E] = []
+            self.adj_list_to_E[street.E].append(street_queue_couple)
+        self.vertices = self.adj_list_from_B.keys()
         self.edges = edges
 
 def solve(inp, args):
     # TODO: Solve the problem
     random.seed(args['seed'])
     ns = parse(inp)
-
+    for i in rnage
     return '0'
 
 if __name__ == '__main__':
